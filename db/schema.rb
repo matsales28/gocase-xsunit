3199c19517fda8855c5cfa8ff73e1f5ccf5d447e
Matheus Sales <matheus_usales@hotmail.com>
2019-09-18 12:14:06 -0300

    database clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_202438) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "flags", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.bigint "survivor_id"
-    t.integer "flagger_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["survivor_id"], name: "index_flags_on_survivor_id"
-  end
-
-  create_table "locations", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.decimal "latitude", precision: 10, scale: 6
-    t.decimal "longitude", precision: 10, scale: 6
-    t.bigint "survivor_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["survivor_id"], name: "index_locations_on_survivor_id"
-  end
-
-  create_table "survivors", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.string "name"
-    t.string "gender"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "status", default: false
-  end
-
-  add_foreign_key "flags", "survivors"
-  add_foreign_key "locations", "survivors"
 end
